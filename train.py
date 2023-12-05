a918f15853d0e5c6e14ee61bd3af4ddf0c683aec
Arnaud <acroisie@student.42lyon.fr>
2023-12-05 18:23:01 +0100

    Fix import typo and normalize data

--- train.py
+++ train.py
@@ -1,28 +1,33 @@
 import csv
-import matplotib.pyplot as plt
+import matplotlib.pyplot as plt
 
-#Read data from csv file
+# Read data from csv file
 mileages = []
 prices = []
 
 with open('data.csv', 'r') as file:
-	reader = csv.reader(file)
-	for row in reader:
-		mileages.append(float(row[0]))
-		prices.append(float(row[1]))
+    dataset = csv.reader(file)
+    next(dataset)
+    for row in dataset:
+        mileages.append(float(row[0]))
+        prices.append(float(row[1]))
 
-#Initialize parameters
+# Initialize parameters
 theta0 = 0
 theta1 = 0
 learning_rate = 0.1
 iterations = 1000
 m = len(mileages)
 
-#Model function
-def model(mileage):
-	return theta0 + theta1 * mileage
+# Normalize data
+mileages = [(m - min(mileages)) / (max(mileages) - min(mileages)) for m in mileages]
+prices = [(p - min(prices)) / (max(prices) - min(prices)) for p in prices]
 
-#Gradient descent
+# Model function
+def model(mileage):
+    return theta0 + theta1 * mileage
+
+# Gradient descent
 for _ in range(iterations):
     tmp_theta0 = (1 / m) * sum(model(mileages[i]) - prices[i] for i in range(m))
     tmp_theta1 = (1 / m) * sum((model(mileages[i]) - prices[i]) * mileages[i] for i in range(m))
@@ -30,12 +35,12 @@ for _ in range(iterations):
     theta0 -= learning_rate * tmp_theta0
     theta1 -= learning_rate * tmp_theta1
 
-#Save parameters
-with open('parameters.txt', 'w') as param_file:
-	param_file.write(str(theta0) + '\n')
-	param_file.write(str(theta1))
+# Save parameters
+with open('theta.txt', 'w') as file:
+    file.write(str(theta0) + '\n')
+    file.write(str(theta1))
 
-#Bonus stuff
+# Bonus stuff
 plt.scatter(mileages, prices, label='Actual Data')
 plt.plot(mileages, [model(m) for m in mileages], color='red', label='Linear Regression')
 plt.xlabel('Mileage')
