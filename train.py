7721c321acb103646e530d304b790c0f66ad269c
Arnaud <acroisie@student.42lyon.fr>
2023-12-05 18:45:22 +0100

    Add cost function and plot cost history

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ theta1 = 0
 learning_rate = 0.1
 iterations = 1000
 m = len(mileages)
+costs_history = []
 
 # Normalize data
 mileages = [(m - min(mileages)) / (max(mileages) - min(mileages)) for m in mileages]
@@ -27,6 +28,10 @@ prices = [(p - min(prices)) / (max(prices) - min(prices)) for p in prices]
 def model(mileage):
     return theta0 + theta1 * mileage
 
+# Cost function
+def cost():
+    return (1 / (2 * m)) * sum((model(mileages[i]) - prices[i]) ** 2 for i in range(m))
+
 # Gradient descent
 for _ in range(iterations):
     tmp_theta0 = (1 / m) * sum(model(mileages[i]) - prices[i] for i in range(m))
@@ -35,15 +40,26 @@ for _ in range(iterations):
     theta0 -= learning_rate * tmp_theta0
     theta1 -= learning_rate * tmp_theta1
 
+    costs_history.append(cost())
+
 # Save parameters
 with open('theta.txt', 'w') as file:
     file.write(str(theta0) + '\n')
     file.write(str(theta1))
 
 # Bonus stuff
+plt.subplot(1, 2, 1)
 plt.scatter(mileages, prices, label='Actual Data')
 plt.plot(mileages, [model(m) for m in mileages], color='red', label='Linear Regression')
 plt.xlabel('Mileage')
 plt.ylabel('Price')
 plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(range(iterations), costs_history, label='Cost History')
+plt.xlabel('Iteration')
+plt.ylabel('Cost')
+plt.legend()
+
+plt.tight_layout()
 plt.show()
