d687ce5fcb8f9f6972699c0a5fd44ca75ba903e7
Arnaud <acroisie@student.42lyon.fr>
2023-12-05 12:13:54 +0100

    first main in python

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def main():
+	mileage = float(input("Enter the mileage of the car: "))
+	print("Mileage:", mileage)
+
+if __name__ == "__main__":
+	main()
+
