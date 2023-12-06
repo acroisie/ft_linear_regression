8344da7608a81f94dd74d09d0c3a25f13b660da4
Arnaud <acroisie@student.42lyon.fr>
2023-12-06 10:55:05 +0100

    Refactor predict.py to use csv module for data reading and add input validation

--- predict.py
+++ predict.py
@@ -1,15 +1,50 @@
-import train.py
+import csv
 
+def model(mileage, theta0, theta1):
+    return theta0 + theta1 * mileage
+
+def normalize(value, min_value, max_value):
+    return (value - min_value) / (max_value - min_value)
+
+def denormalize(value, min_value, max_value):
+    return value * (max_value - min_value) + min_value
+
+# Load parameters
 with open('theta.txt', 'r') as file:
-	theta0 = float(file.readline())
-	theta1 = float(file.readline())
+    lines = file.readlines()
+    theta0 = float(lines[0])
+    theta1 = float(lines[1])
 
-print('Theta0: ' + str(theta0))
-print('Theta1: ' + str(theta1))
+# Read data from csv file
+mileages = []
+prices = []
 
+with open('data.csv', 'r') as file:
+    dataset = csv.reader(file)
+    next(dataset)
+    for row in dataset:
+        mileages.append(float(row[0]))
+        prices.append(float(row[1]))
+
+min_mileage = min(mileages)
+max_mileage = max(mileages)
+
+# Interactive prompt
 while True:
-	mileage = input('Enter a mileage: ')
-	if mileage == 'exit':
-		break
-	print('Price: ' + str(theta0 + theta1 * float(mileage)))
+    mileage = input("Enter a mileage: ")
+    try:
+        mileage = float(mileage)
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
 
+    # Normalize input
+    mileage_norm = normalize(mileage, min_mileage, max_mileage)
+
+    # Predict price
+    price_norm = model(mileage_norm, theta0, theta1)
+
+    # Denormalize output
+    price = denormalize(price_norm, min(prices), max(prices))
+
+    print(f"The estimated price for a car with {mileage} km is {price} euros.")
